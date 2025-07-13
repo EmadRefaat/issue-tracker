5605de840feb3e4a9e42e132e1620458507d0cf3
emad refaat <emad.refaat57@gmial.com>
2025-07-13 19:35:00 +0300

    Add additional loading skeltons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Box, Flex } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const IssueDetailLoadingPage = () => {
-  return <div>loadin...</div>;
+  return (
+    <Box className="max-w-2xl ">
+      <Skeleton />
+      <Flex gap="2" my="2">
+        <Skeleton width="2rem" />
+        <Skeleton width="4rem" />
+      </Flex>
+      <Skeleton count={5} />
+    </Box>
+  );
 };
 
 export default IssueDetailLoadingPage;
--- app/issues/newissue/loading.tsx
+++ app/issues/newissue/loading.tsx
@@ -1,7 +1,21 @@
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+import { TextField, TextFieldInput, Button, Box } from "@radix-ui/themes";
+import { error } from "console";
 import React from "react";
+import { Controller } from "react-hook-form";
+import loading from "../loading";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const NewIssueLoading = () => {
-  return <div>loading</div>;
+  return (
+    <Box className="max-w-xl ">
+      <Skeleton />
+      <Skeleton height={"25rem"} />
+      <Skeleton width={"5rem"} height="2rem" className="mt-3" />
+    </Box>
+  );
 };
 
 export default NewIssueLoading;
