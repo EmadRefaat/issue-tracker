0c6882ef472029576868194f7ef9d4ae204c66d9
emad refaat <emad.refaat57@gmial.com>
2025-07-15 14:16:42 +0300

    Building api for update issue

--- app/ValidationSchema.tsx
+++ app/ValidationSchema.tsx
@@ -1,6 +1,6 @@
 import z from "zod";
 
-export const issueSchema = z.object({
+export const ValidationSchema = z.object({
   title: z.string().min(1, "min title length is one character").max(250),
   description: z.string().min(1, "min description length is one character"),
 });
