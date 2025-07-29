8104d09f042acae9fc63a9b2e9b71a420d40770a
emad refaat <emad.refaat57@gmial.com>
2025-07-29 13:33:27 +0300

    Change the layout of the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import { usePathname } from "next/navigation";
 import classnames from "classnames";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
 const Navbar = () => {
   const { data: session, status } = useSession();
@@ -16,32 +16,39 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="flex space-x-4 border-b-2 border-zinc-200 h-14 items-center px-5 transition-colors ">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-4 ">
-        {titles.map((item) => (
-          <li
-            className={classnames({
-              "text-zinc-900": item.href === pathname,
-              "text-zinc-400": item.href !== pathname,
-              "hover:text-zinc-800 transition-colors": true,
-            })}
-            key={item.href}
-          >
-            <Link href={item.href}>{item.title}</Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Logout</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Login</Link>
-        )}
-      </Box>
+    <nav className=" space-x-4 border-b-2 border-zinc-200 py-3 items-center px-5 transition-colors ">
+      <Container>
+        <Flex justify="between">
+          <Flex align={"center"} gap="3">
+            {" "}
+            <Link href="/">
+              <AiFillBug />
+            </Link>
+            <ul className="flex space-x-4 ">
+              {titles.map((item) => (
+                <li
+                  className={classnames({
+                    "text-zinc-900": item.href === pathname,
+                    "text-zinc-400": item.href !== pathname,
+                    "hover:text-zinc-800 transition-colors": true,
+                  })}
+                  key={item.href}
+                >
+                  <Link href={item.href}>{item.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Logout</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
