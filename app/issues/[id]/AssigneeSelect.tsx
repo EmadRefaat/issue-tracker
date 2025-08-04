ba8aec8152b17a3e7f98e24fea20cd5f1d990591
emad refaat <emad.refaat57@gmial.com>
2025-08-04 22:42:53 +0300

    Refactor assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,11 +1,10 @@
 "use client";
-import { Select } from "@radix-ui/themes";
-import axios from "axios";
-import { User } from "next-auth";
-import React, { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
 import Skeleton from "@/app/components/Skeleton";
 import { Issue } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { User } from "next-auth";
 import toast, { Toaster } from "react-hot-toast";
 const fetchUsers = async () => {
   const { data } = await axios.get("/api/users");
@@ -15,32 +14,33 @@ const fetchUsers = async () => {
 interface props {
   issue: Issue;
 }
-const AssgineeSelect = ({ issue }: props) => {
-  const {
-    data: users = [],
-    isLoading,
-    error,
-  } = useQuery<User[]>({
+
+const useUsers = () =>
+  useQuery<User[]>({
     queryKey: ["users"],
     queryFn: fetchUsers,
     staleTime: 60 * 1000,
     retry: 3,
   });
+const AssgineeSelect = ({ issue }: props) => {
+  const assignUser = (userId: string) => {
+    return axios
+      .patch("/api/issues/" + issue.id, {
+        assignedToUserId: userId || null,
+      })
+      .catch((err) => {
+        toast.error("changes could not be saved");
+      });
+  };
+
+  const { data: users = [], isLoading, error } = useUsers();
   if (isLoading) return <Skeleton></Skeleton>;
   if (error) return null;
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || ""}
-        onValueChange={(userId) =>
-          axios
-            .patch("/api/issues/" + issue.id, {
-              assignedToUserId: userId || null,
-            })
-            .catch((err) => {
-              toast.error("changes could not be saved");
-            })
-        }
+        onValueChange={(userId) => assignUser(userId)}
       >
         <Select.Trigger placeholder="Assignee" />
         <Select.Content>
