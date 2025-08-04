75710fc126fd0f8ea625ea0e0c28e65b6dfb1d75
emad refaat <emad.refaat57@gmial.com>
2025-08-04 21:24:46 +0300

    Show toast notification

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import Skeleton from "@/app/components/Skeleton";
 import { Issue } from "@prisma/client";
-
+import toast, { Toaster } from "react-hot-toast";
 const fetchUsers = async () => {
   const { data } = await axios.get("/api/users");
   return Array.isArray(data) ? data : data.users ?? [];
@@ -29,27 +29,34 @@ const AssgineeSelect = ({ issue }: props) => {
   if (isLoading) return <Skeleton></Skeleton>;
   if (error) return null;
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || ""}
-      onValueChange={(userId) =>
-        axios.patch("/api/issues/" + issue.id, {
-          assignedToUserId: userId || null,
-        })
-      }
-    >
-      <Select.Trigger placeholder="Assignee" />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestion</Select.Label>
-          <Select.Item value="">Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={(userId) =>
+          axios
+            .patch("/api/issues/" + issue.id, {
+              assignedToUserId: userId || null,
+            })
+            .catch((err) => {
+              toast.error("changes could not be saved");
+            })
+        }
+      >
+        <Select.Trigger placeholder="Assignee" />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestion</Select.Label>
+            <Select.Item value="">Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster position="top-center" />
+    </>
   );
 };
 
